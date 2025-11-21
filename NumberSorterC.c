c080b381c25dbaf9d526835dd7110e287425cb63
Matt Manire <153966798+u31899@users.noreply.github.com>
2025-11-21 10:27:27 -0800

    Update NumberSorterC.c

--- NumberSorterC.c
+++ NumberSorterC.c
@@ -30,7 +30,7 @@ Will need the following functions in the program:
 
 
 int number_list[11] = {0,1,2,3,4,5,6,7,8,9, 10};
-int nll = 11;
+int nll = 11; // number length list
 int count_by_2 = 0;
 int count_by_3 = 0;
 int count_by_4 = 0;
@@ -48,7 +48,23 @@ void main(void) {
 		printf("%d, ", number_list[i]);
 		}
 	}
-	
+	//Scan through the array to count multiples of different values
+	for(i = 0; i < nll; i++){
+		if(number_list[i] % 2 == 0){
+			count_by_2++;
+		}
+		if(number_list[i] % 3 == 0){
+			count_by_3++;
+		}
+		if(number_list[i] % 4 == 0){
+			count_by_4++;
+		}
+		if(number_list[i] % 5 == 0){
+			count_by_5++;
+		}
+		
+	}
+	printf("%d %d %d %d", count_by_2, count_by_3, count_by_4, count_by_5);
 	
 	while(1);
 
